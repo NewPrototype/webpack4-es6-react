49e0c031f0e191973a40411917de474cc5ccf5e3
heicao <heicao@heicao.local>
2018-10-20 00:05:25 +0800

    add

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -19,14 +19,6 @@ class Home extends React.Component {
 		</div>
     );
   }
-
-  componentWillMount() { }
-  componentDidMount() { }
-  componentWillReceiveProps(nextProps) { }
-  shouldComponentUpdate(nextProps, nextState) { return true; }
-  componentWillUpdate(nextProps, nextState) { }
-  componentDidUpdate(prevProps, prevState) { }
-  componentWillUnmount() { }
 }
 
 export default Home;
